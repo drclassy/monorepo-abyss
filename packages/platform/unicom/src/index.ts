82996f9a46e0a7e87d8de22e7820e0b61f7a0161
Classy + <240677945+Avvicenna@users.noreply.github.com>
2026-05-20 01:35:59 +0700

    fix(unicom): remove broken server export to unblock global build

--- packages/platform/unicom/src/index.ts
+++ packages/platform/unicom/src/index.ts
@@ -2,4 +2,4 @@
 export type { AgentEntry, UNICOMMessage, AgentStatus } from './types.js'
 export { AgentRegistry } from './registry.js'
 export { MessageInbox } from './inbox.js'
-export { createUnicomHttpServer } from './server.js'
+export { routeMessage } from './router.js'
