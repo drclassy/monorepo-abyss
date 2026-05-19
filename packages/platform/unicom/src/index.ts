4f87b655252830930530eb811c29c59e53f604a2
Classy + <240677945+Avvicenna@users.noreply.github.com>
2026-05-20 01:00:53 +0700

    chore(unicom): scaffold @the-abyss/unicom package

---
+++ packages/platform/unicom/src/index.ts
@@ -0,0 +1,5 @@
+// packages/platform/unicom/src/index.ts
+export type { AgentEntry, UNICOMMessage, AgentStatus } from './types.js'
+export { AgentRegistry } from './registry.js'
+export { MessageInbox } from './inbox.js'
+export { createUnicomHttpServer } from './server.js'
